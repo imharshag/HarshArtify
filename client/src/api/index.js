15e5f1ec8e873ba86afb4565475c0672683e2742
imharshag <harshag3106@gmail.com>
2024-08-05 19:05:51 +0530

    netlify

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "",
+  baseURL: "https://harshartify.onrender.com/api/",
 });
 
 export const GetPosts = async () => await API.get("/post/");
